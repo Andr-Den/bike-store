68a8e99fe34dd77a68cbccabadf4cc4dc17b4ab1
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-16 14:49:45 +0500

    add disabled for button

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,15 +2,19 @@ const inputElement = document.getElementById('form-input')
 const buttonElement = document.getElementById('submit-button')
 
 if (inputElement.validity.valid === true) { 
-  buttonElement.classList.remove('form__submit-button_no-active'); 
+  buttonElement.classList.remove('form__submit-button_no-active');
+  buttonElement.disabled = ""; 
 } else { 
-  buttonElement.classList.add('form__submit-button_no-active'); 
+  buttonElement.classList.add('form__submit-button_no-active');
+  buttonElement.disabled = "disabled"; 
 }
 
 inputElement.addEventListener('input', function () { 
   if (inputElement.validity.valid === true) {
-    buttonElement.classList.remove('form__submit-button_no-active'); 
+    buttonElement.classList.remove('form__submit-button_no-active');
+    buttonElement.disabled = "";
   } else {
-    buttonElement.classList.add('form__submit-button_no-active'); 
+    buttonElement.classList.add('form__submit-button_no-active');
+    buttonElement.disabled = "disabled";
   }
 }); 
